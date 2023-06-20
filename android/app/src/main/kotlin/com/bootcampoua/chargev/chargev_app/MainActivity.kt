79e798e0d492aca2f959eb7b1214cc08de1b364f
guvennmehmet <guvennmehmet37@gmail.com>
2023-06-20 12:37:55 +0300

    FlutterFire ve Firebase CLI Kurulumları

--- android/app/src/main/kotlin/com/bootcampoua/chargev/chargev_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.bootcampoua.chargev.chargev_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:chargevapp/firebase_options.dart';
 import 'package:chargevapp/widgets/appbar.dart';
 import 'package:chargevapp/widgets/navbar.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
