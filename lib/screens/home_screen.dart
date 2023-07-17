f900aa5bb4e09f9412181e732f0952f41268c95a
guvennmehmet <guvennmehmet37@gmail.com>
2023-07-17 22:52:14 +0300

    son

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -14,21 +14,117 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Drawer(),
-      appBar: AppBar(
-
-          elevation: 0,
-          backgroundColor: Colors.grey[300],
-          title: Text(
-            "chargEV",
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 20,
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(color: const Color(0xff4280ef)),
+              child: Center(
+                child: Text(
+                  'ChargEV',
+                  style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
             ),
-          ),
-          centerTitle: true,
+            ListTile(
+              leading: Icon(
+                Icons.home,
+              ),
+              title: const Text(
+                'Ana Sayfa',
+                style: TextStyle(
+                    fontSize: 25,
+                    color: Color(0xff787878),
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.add_alarm_sharp,
+              ),
+              title: const Text(
+                'Şarj Planlama',
+                style: TextStyle(
+                    fontSize: 25,
+                    color: Color(0xff787878),
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.flutter_dash_outlined),
+              title: const Text(
+                'Mesafe Planlama',
+                style: TextStyle(
+                    fontSize: 25,
+                    color: Color(0xff787878),
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.gamepad_outlined,
+              ),
+              title: const Text('Yönlendirme',
+                  style: TextStyle(
+                      fontSize: 25,
+                      color: Color(0xff787878),
+                      fontWeight: FontWeight.bold)),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.book,
+              ),
+              title: const Text('Hakkımda',
+                  style: TextStyle(
+                      fontSize: 25,
+                      color: Color(0xff787878),
+                      fontWeight: FontWeight.bold)),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.business_sharp,
+              ),
+              title: const Text('Mobil Şarj',
+                  style: TextStyle(
+                      fontSize: 25,
+                      color: Color(0xff787878),
+                      fontWeight: FontWeight.bold)),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.today,
+              ),
+              title: const Text('Bugün Ne Var?',
+                  style: TextStyle(
+                      fontSize: 25,
+                      color: Color(0xff787878),
+                      fontWeight: FontWeight.bold)),
+            ),
+          ],
+        ),
       ),
-      body: Center(child: Text("HOME")),
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.grey[300],
+        title: const Text(
+          "chargEV",
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 20,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: const Image(
+          image: NetworkImage(
+              'https://www.pngmart.com/files/4/Tesla-PNG-Image.png'),
+        ),
+      ),
     );
   }
 }
